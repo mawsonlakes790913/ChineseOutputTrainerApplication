c32eca2ff09fc8a57271883948b4b63f0886907a
mawsonlakes790913 <liuniao790913@gmail.com>
2026-08-12 20:22:27 +0900

    chore: set up development environment

---
+++ chinese-output-forge/src/main/java/io/github/mawsonlakes790913/chineseoutputforge/ChineseOutputForgeApplication.java
@@ -0,0 +1,13 @@
+package io.github.mawsonlakes790913.chineseoutputforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChineseOutputForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChineseOutputForgeApplication.class, args);
+	}
+
+}
---
+++ chinese-output-forge/src/test/java/io/github/mawsonlakes790913/chineseoutputforge/ChineseOutputForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mawsonlakes790913.chineseoutputforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChineseOutputForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
