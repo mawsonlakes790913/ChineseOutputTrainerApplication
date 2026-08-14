bfa29d776be5b18e6422eeeccd9d21ae1dc800bb
mawsonlakes790913 <liuniao790913@gmail.com>
2026-08-15 04:28:41 +0900

    feat: add practice question page

---
+++ chinese-output-forge/src/main/resources/static/js/practice.js
@@ -0,0 +1,8 @@
+function showAnswer() {
+
+    const answerButton = document.getElementById("answerButton");
+    const answerArea = document.getElementById("answerArea");
+
+    answerButton.style.display = "none";
+    answerArea.style.display = "block";
+}
