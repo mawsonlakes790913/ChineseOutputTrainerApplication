6281a907335290e545d0493f834eac8a118205f1
mawsonlakes790913 <liuniao790913@gmail.com>
2026-09-13 02:38:24 +0900

    feat: add multilingual about page

--- chinese-output-forge/src/main/java/io/github/mawsonlakes790913/chineseoutputforge/controller/HomeController.java
+++ chinese-output-forge/src/main/java/io/github/mawsonlakes790913/chineseoutputforge/controller/HomeController.java
@@ -1,5 +1,7 @@
 package io.github.mawsonlakes790913.chineseoutputforge.controller;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +25,19 @@ public class HomeController {
 	    return "/home";
 	}
 	
-    @GetMapping("/about")
-    public String getTutorial() {
-        return "/about";
-    }
+	@GetMapping("/about")
+	public String getAbout(Locale locale) {
+
+	    if (Locale.SIMPLIFIED_CHINESE.equals(locale)) {
+	        return "about/about-zh-cn";
+	    }
+
+	    if (Locale.TRADITIONAL_CHINESE.equals(locale)) {
+	        return "about/about-zh-tw";
+	    }
+
+	    return "about/about";
+	}
+    
 
 }
